6fa9bfe54d96aa2a6587a2dac0544c4262afdf43
Raffy <raffyjay_dayag@yahoo.com>
2023-09-28 22:49:18 +0800

    added HTMLWebpackPlugin. finish other pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { generateHomepage } from "./pages/home.js";
 import { generateMenu } from "./pages/menu.js";
-import { generateNavbar } from "./navbar.js";
+import { generateNavbar } from "./pages/navbar.js";
 import './css/styles.css';
 import './css/menu.css';
 import background from './images/homepage.jpg'
