c717ff06110e0b01c6b59ff03244a36d2d471671
Raffy <raffyjay_dayag@yahoo.com>
2023-10-03 22:27:22 +0800

    contact page wip

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { generateHomepage } from "./pages/home.js";
 import { generateMenu } from "./pages/menu.js";
 import { generateNavbar } from "./pages/navbar.js";
 import './css/styles.css';
-import background from './images/homepage.jpg'
 
 generateNavbar();
 generateHomepage();
@@ -10,18 +9,15 @@ generateHomepage();
 const home = document.getElementById("home");
 const menu =  document.getElementById("menu");
 const body = document.body;
-const backgroundSrc = background;
 
 home.addEventListener("click", () => {
     body.textContent = "";
-    body.style.background = `url(${backgroundSrc}) no-repeat fixed center/cover`;
     generateNavbar();
     generateHomepage();
 })
 
 menu.addEventListener("click", () => {
     body.textContent = "";
-    body.style.background = "none";
     generateNavbar();
     generateMenu();
 });
